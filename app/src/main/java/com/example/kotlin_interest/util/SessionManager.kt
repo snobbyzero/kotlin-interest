74772cf5bddb9335cec7bbb6f145a94a5e672564
Ildar Magzhanov <ildar200103@rambler.ru>
2020-08-05 17:27:31 +0300

    now device uuid and information about login stores

--- app/src/main/java/com/example/kotlin_interest/util/SessionManager.kt
+++ app/src/main/java/com/example/kotlin_interest/util/SessionManager.kt
@@ -5,19 +5,24 @@ import android.content.SharedPreferences
 import com.example.kotlin_interest.model.JwtResponse
 import com.example.kotlin_interest.model.User
 import com.google.gson.Gson
+import java.util.*
 import javax.inject.Inject
 import javax.inject.Singleton
 
 class SessionManager @Inject constructor(private val sharedPreferences: SharedPreferences, private val gson: Gson) {
 
-    fun getAccessToken() : String? = sharedPreferences.getString(ACCESS_TOKEN, null)
+    fun getAccessToken() : String = sharedPreferences.getString(ACCESS_TOKEN, "-")!!
 
-    fun getRefreshToken() : String? = sharedPreferences.getString(REFRESH_TOKEN, null)
+    fun getRefreshToken() : String = sharedPreferences.getString(REFRESH_TOKEN, "-")!!
+
+    fun getFingerprint() : String = sharedPreferences.getString(FINGERPRINT, createAndSaveFingerprint())!!
 
     fun getUser() : User? {
-        val json = sharedPreferences.getString(USER, null);
+        val json = sharedPreferences.getString(USER, null)
         json.let { return gson.fromJson(it, User::class.java) }
     }
+
+    fun getLoggedIn() : Boolean = sharedPreferences.getBoolean(LOGGED_IN, false)
 
     fun saveTokens(jwtResponse: JwtResponse) {
         val editor = sharedPreferences.edit()
@@ -26,11 +31,27 @@ class SessionManager @Inject constructor(private val sharedPreferences: SharedPr
         editor.apply()
     }
 
-    fun saveUser(user: User) = sharedPreferences.edit().putString(USER, gson.toJson(user))
+    fun saveUser(user: User) = sharedPreferences.edit().putString(USER, gson.toJson(user)).apply()
+
+    private fun createAndSaveFingerprint() : String {
+        val fingerprint = UUID.randomUUID().toString()
+        if (!sharedPreferences.contains(FINGERPRINT)) {
+            val editor = sharedPreferences.edit()
+            editor.putString(FINGERPRINT, fingerprint)
+            editor.apply()
+        }
+        return fingerprint
+    }
+
+    fun setLoggedIn(isLoggedIn: Boolean) {
+        sharedPreferences.edit().putBoolean(LOGGED_IN, isLoggedIn).apply()
+    }
 
     companion object {
         const val ACCESS_TOKEN = "ACCESS_TOKEN"
         const val REFRESH_TOKEN = "REFRESH_TOKEN"
         const val USER = "USER"
+        const val FINGERPRINT = "FINGERPRINT"
+        const val LOGGED_IN = "LOGGED_IN"
     }
 }
