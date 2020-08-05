60a9c59dfb4859a499574bb3371647085e6f2010
Ildar Magzhanov <ildar200103@rambler.ru>
2020-08-05 17:22:51 +0300

    TokenAuthenticator now works

--- app/src/main/java/com/example/kotlin_interest/retrofit/TokenAuthenticator.kt
+++ app/src/main/java/com/example/kotlin_interest/retrofit/TokenAuthenticator.kt
@@ -1,5 +1,12 @@
 package com.example.kotlin_interest.retrofit
 
+import com.example.kotlin_interest.model.JwtResponse
+import com.example.kotlin_interest.util.SessionManager
+import dagger.Lazy
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
 import okhttp3.Authenticator
 import okhttp3.Request
 import okhttp3.Response
@@ -8,12 +15,26 @@ import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
-class TokenAuthenticator: Authenticator {
-    @Inject lateinit var loginRetrofitService: LoginRetrofitService
+class TokenAuthenticator @Inject constructor(val sessionManager: SessionManager, val loginRetrofitService: Lazy<LoginRetrofitService>): Authenticator {
 
     override fun authenticate(route: Route?, response: Response): Request? {
-        newJwt =
-        //this is your new token
-        return response.request().newBuilder().header("Authorization", "Bearer " + newToken).build()
+        val token = runBlocking { getNewToken() }
+        return response.request().newBuilder().header("Authorization", "Bearer " + token?.accessToken).build()
+    }
+
+
+    private suspend fun getNewToken() : JwtResponse? {
+        var token: JwtResponse? = null
+        withContext(CoroutineScope(Dispatchers.IO).coroutineContext) {
+            val jwtResponse = loginRetrofitService.get().updateTokens(
+                sessionManager.getRefreshToken()!!,
+                sessionManager.getAccessToken()!!
+            )
+            if (jwtResponse.isSuccessful) {
+                token = jwtResponse.body()
+                sessionManager.saveTokens(token!!)
+            }
+        }
+        return token
     }
 }
